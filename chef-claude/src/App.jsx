747da0c1381f6be72026da28445acc522e9d0e74
Kachi Maxxxxx <kachimax0101@gmail.com>
2025-06-20 15:32:21 +0100

    useState setterr func implemented

--- chef-claude/src/App.jsx
+++ chef-claude/src/App.jsx
@@ -1,18 +1,40 @@
-import {useState} from "react";
+import { useState } from "react";
 
-export default function App(){
-  const [isImportant, setIsImportant] = useState("Yes");
-  console.log(isImportant);
- function handleClick(){
-  setIsImportant("Definitely")
- }
-  
-
-return (
-  <main>
-    <h1 className="title">Is state important to you?</h1>
-    <button onClick={handleClick} className="value">{isImportant}</button>
-  </main>
-)
-
+export default function App() {
+  const [count, setCount] = useState(0);
+  function handleMinus() {
+    setCount((prevCount) => prevCount - 1);
+  }
+  function handlePlus() {
+    setCount(function (prevCount) {
+      return prevCount + 1;
+    });
+  }
+  return (
+    <main className="container">
+      <h1 className="title">
+        How many times will Bob say "State" in this comment section?
+      </h1>
+      <div className="counter">
+        <button
+          onClick={handleMinus}
+          className="minus"
+          aria-label="Decrease Count"
+        >
+          {" "}
+          -{" "}
+        </button>
+        <h2 className="count"> {count}</h2>
+        <button
+          onClick={handlePlus}
+          className="plus"
+          aria-label="Increase Count"
+        >
+          {" "}
+          +{" "}
+        </button>
+      </div>
+    </main>
+  );
 }
+
