99edda0783893813a7b563a9284b82bfdc8154bb
Kachi Maxxxxx <kachimax0101@gmail.com>
2025-06-26 00:07:37 +0100

    toggle state implemented

--- chef-claude/src/App.jsx
+++ chef-claude/src/App.jsx
@@ -1,40 +1,24 @@
-import { useState } from "react";
+import {useState} from "react";
 
-export default function App() {
-  const [count, setCount] = useState(0);
-  function handleMinus() {
-    setCount((prevCount) => prevCount - 1);
+
+export default function App(){
+ 
+
+const [isGoingOut, setIsGoingOut] = useState(true)
+  function handleClick(){
+    setIsGoingOut(active=> !active)
   }
-  function handlePlus() {
-    setCount(function (prevCount) {
-      return prevCount + 1;
-    });
-  }
-  return (
-    <main className="container">
-      <h1 className="title">
-        How many times will Bob say "State" in this comment section?
-      </h1>
-      <div className="counter">
-        <button
-          onClick={handleMinus}
-          className="minus"
-          aria-label="Decrease Count"
-        >
-          {" "}
-          -{" "}
-        </button>
-        <h2 className="count"> {count}</h2>
-        <button
-          onClick={handlePlus}
-          className="plus"
-          aria-label="Increase Count"
-        >
-          {" "}
-          +{" "}
-        </button>
-      </div>
-    </main>
-  );
+  return(
+  <main>
+    <h1>Do I feel like going out today?</h1>
+    <button onClick={handleClick}>{isGoingOut===true?"Yes": "No"}</button>
+  </main>
+ )
+
+
+
+
+
+
+
 }
-
