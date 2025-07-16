f5e87aeda95aa23612e032c823d3ad1a30b59e76
Kachi Maxxxxx <kachimax0101@gmail.com>
2025-07-16 16:25:36 +0100

    chore: track renamed client folder

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <header>
+            <div className="navbar">
+             <img src="/src/assets/Chef Claude Icon.svg" alt="chef icon" className="nav__img"/> 
+             <p className="nav__text">Chef Claude</p>  
+            </div>
+        </header>
+    )
+}
